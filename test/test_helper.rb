bee93425753fcc54b91d250d93ec4273a1167508
Michal Cichra <michal@cichra.cz>
2017-06-01 09:37:28 +0200

    integrate circle ci

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,11 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+if ENV.key?('CI')
+  require 'minitest/reporters/junit_reporter'
+  Minitest.reporter << Minitest::Reporters::JUnitReporter.new
+end
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all

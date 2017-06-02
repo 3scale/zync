689e8fd82c5d582e543d2f6c9e4761806f5ff353
Michal Cichra <michal@cichra.cz>
2017-06-02 14:53:10 +0200

    create service generator

---
+++ lib/generators/service/service_generator.rb
@@ -0,0 +1,12 @@
+class ServiceGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('../templates', __FILE__)
+  check_class_collision suffix: 'Service'
+
+  def copy_service_file
+    template 'service.rb.erb', "app/services/#{file_name}_service.rb"
+  end
+
+  def copy_service_test_file
+    template 'service_test.rb.erb', "test/services/#{file_name}_service_test.rb"
+  end
+end

97535a2596a4b5ad4944aa027bf28c0598a0c928
Michal Cichra <michal@cichra.cz>
2017-06-06 10:27:10 +0200

    extract notification data to the model

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,30 @@
 class Notification < ApplicationRecord
   belongs_to :model
   belongs_to :tenant
+
+  # Wrapper extracting information from incoming notification.
+  class NotificationData
+    def initialize(data)
+      @data = ActiveSupport::HashWithIndifferentAccess.new(data)
+    end
+
+    def type
+      # FIXME: do this safely
+      @data.fetch(:type).constantize
+    end
+
+    def to_hash
+      @data.except(:type, :tenant_id)
+    end
+  end
+  private_constant :NotificationData
+
+  def create_model
+    data = NotificationData.new(self.data)
+
+    type = data.type.find_or_create_by!(data.to_hash.merge(tenant: tenant))
+
+    Model.find_or_create_by!(record: type, tenant: tenant)
+  end
+
 end
--- app/services/incoming_notification_service.rb
+++ app/services/incoming_notification_service.rb
@@ -6,36 +6,9 @@ class IncomingNotificationService
     freeze
   end
 
-  # Wrapper extracting information from incoming notification.
-  class NotificationData
-    def initialize(data)
-      @data = ActiveSupport::HashWithIndifferentAccess.new(data)
-    end
-
-    def type
-      # FIXME: do this safely
-      @data.fetch(:type).constantize
-    end
-
-    def to_hash
-      @data.except(:type, :tenant_id)
-    end
-  end
-  private_constant :NotificationData
-
-  def extract_model(notification)
-    data = NotificationData.new(notification.try(:data))
-
-    tenant = notification.tenant
-
-    type = data.type.find_or_create_by!(data.to_hash.merge(tenant: tenant))
-
-    Model.find_or_create_by!(record: type, tenant: tenant)
-  end
-
   def call(notification)
     notification.transaction do
-      model = notification.model ||= extract_model(notification)
+      model = notification.model ||= notification.create_model
 
       notification.save!
 

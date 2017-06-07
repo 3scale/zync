a16ec7da30a108327fa7380394e08ce63c5b1ced
Michal Cichra <michal@cichra.cz>
2017-06-07 08:35:33 +0200

    integrate responders gem

---
+++ lib/application_responder.rb
@@ -0,0 +1,13 @@
+# Main Responder used by the ApplicationController
+# It is used to handle the #respond_with responses
+
+class ApplicationResponder < ActionController::Responder
+
+  def api_behavior
+    if put?
+      display resource, status: :created
+    else
+      super
+    end
+  end
+end

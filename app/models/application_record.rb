0bc7510ee4777d190f25e613f9859313c1e9bd96
Michal Cichra <michal@cichra.cz>
2017-06-13 16:34:16 +0200

    improve logging when retrying creating records

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,4 +1,23 @@
 # frozen_string_literal: true
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.retry_record_not_unique
+    retried = false
+
+    begin
+      transaction(requires_new: true) do
+        yield
+      end
+    rescue ActiveRecord::RecordNotUnique => error
+      logger.warn(error) { "[#{self}][#{error.class}] Retrying after #{error}" }
+
+      unless retried
+        retried = true
+        retry
+      end
+    end
+  end
+
+  delegate :retry_record_not_unique, to: :class
 end

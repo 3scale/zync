9fdebd119395fc64a20ab52c5753b00b28ecd9e1
Michal Cichra <michal@cichra.cz>
2017-07-18 13:57:54 +0200

    cleanup subscriber base into shared module

--- lib/lograge/job_log_subscriber.rb
+++ lib/lograge/job_log_subscriber.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
+require 'lograge/subscriber_base'
+
 module Lograge
+  # Log Subscriber for ActiveJob. Replaces Rails default subscriber.
+
   class JobLogSubscriber < ActiveSupport::LogSubscriber
+    # To be included in ActiveJob::Base to turn off tagged logging.
     module Logging
       private
 
@@ -10,52 +15,17 @@ module Lograge
       end
     end
 
-    def enqueue(event)
-      log(event) do
-        payload = event.payload
-        data = extract_event(event, payload)
-        before_format(data, payload)
-      end
-    end
+    include Lograge::SubscriberBase
 
-    def enqueue_at(event)
-      log(event) do
-        payload = event.payload
-        data = extract_event(event, payload)
-        before_format(data, payload)
-      end
-    end
-
-    def perform(event)
-      log(event) do
-        payload = event.payload
-        data = extract_event(event, payload)
-        before_format(data, payload)
-      end
-    end
-
-    def logger
-      Lograge.logger.presence || super
-    end
+    log_event :enqueue
+    log_event :enqueue_at
+    log_event :perform
 
     private
 
-    def log(event)
-      return if Lograge.ignore?(event)
-
-      data = yield
-      formatted_message = Lograge.formatter.call(data)
-      logger.send(Lograge.log_level, formatted_message)
-    end
-
-    def extract_event(event, payload = event.payload)
-      initial_data(payload).tap do |data|
-        data.merge!(extract_action(event))
-        data.merge!(extract_duration(event))
-        data.merge!(extract_error(payload))
-        data.merge!(extract_schedule(payload))
-        data.merge!(custom_options(event))
-      end
+    def merge_data(data, event, payload)
+      super
+      data.merge!(extract_schedule(payload))
     end
 
     def initial_data(payload)
@@ -70,31 +40,10 @@ module Lograge
       }
     end
 
-    EMPTY_DATA = {}.freeze
-    private_constant :EMPTY_DATA
-
-    def extract_error(payload)
-      exception, message = payload.fetch(:exception) { return EMPTY_DATA }
-
-      {
-        exception: exception,
-        error: "#{exception}: #{message}"
-      }
-    end
-
     def extract_schedule(payload)
       scheduled_at = payload.fetch(:job).scheduled_at or return EMPTY_DATA
 
       { scheduled_at: Time.at(scheduled_at).utc }
-    end
-
-    def extract_action(event)
-      { action: event.name.match(/^([^\.]+)/) }
-    end
-
-    def extract_duration(event)
-      duration = event.duration or return EMPTY_DATA
-      { duration: duration.round(2) }
     end
 
     def extract_adapter(payload)
@@ -116,14 +65,6 @@ module Lograge
       else
         arg
       end
-    end
-
-    def before_format(data, payload)
-      Lograge.before_format(data, payload)
-    end
-
-    def custom_options(event)
-      Lograge.custom_options(event) || {}
     end
   end
 end
--- lib/lograge/oidc_subscriber.rb
+++ lib/lograge/oidc_subscriber.rb
@@ -1,53 +1,18 @@
 # frozen_string_literal: true
 
+require 'lograge/subscriber_base'
+
 module Lograge
+  # Log Subscriber for OIDC integrations. Consumes Client create/update/remove events.
+
   class OIDCSubscriber < ActiveSupport::LogSubscriber
-    def create_client(event)
-      log(event) do
-        payload = event.payload
-        data = extract_client(event, payload)
-        before_format(data, payload)
-      end
-    end
+    include Lograge::SubscriberBase
 
-    def update_client(event)
-      log(event) do
-        payload = event.payload
-        data = extract_client(event, payload)
-        before_format(data, payload)
-      end
-    end
+    log_event :create_client
+    log_event :update_client
+    log_event :remove_client
 
-    def remove_client(event)
-      log(event) do
-        payload = event.payload
-        data = extract_client(event, payload)
-        before_format(data, payload)
-      end
-    end
-
-    def logger
-      Lograge.logger.presence || super
-    end
-
-    private
-
-    def log(event)
-      return if Lograge.ignore?(event)
-
-      data = yield
-      formatted_message = Lograge.formatter.call(data)
-      logger.send(Lograge.log_level, formatted_message)
-    end
-
-    def extract_client(event, payload = event.payload)
-      initial_data(payload).tap do |data|
-        data.merge!(extract_action(event))
-        data.merge!(extract_duration(event))
-        data.merge!(extract_error(payload))
-        data.merge!(custom_options(event))
-      end
-    end
+    protected
 
     def initial_data(payload)
       client = payload.fetch(:client)
@@ -58,37 +23,8 @@ module Lograge
       }
     end
 
-    EMPTY_DATA = {}.freeze
-    private_constant :EMPTY_DATA
-
-    def extract_error(payload)
-      exception, message = payload.fetch(:exception) { return EMPTY_DATA }
-
-      {
-        exception: exception,
-        error: "#{exception}: #{message}"
-      }
-    end
-
-    def extract_action(event)
-      { action: event.name.match(/^([^\.]+)/) }
-    end
-
-    def extract_duration(event)
-      duration = event.duration or return EMPTY_DATA
-      { duration: duration.round(2) }
-    end
-
     def extract_adapter(payload)
-      payload.fetch(:adapter).class.name.demodulize.remove('Adapter')
-    end
-
-    def before_format(data, payload)
-      Lograge.before_format(data, payload)
-    end
-
-    def custom_options(event)
-      Lograge.custom_options(event) || {}
+      payload.fetch(:adapter).class.name
     end
   end
 end

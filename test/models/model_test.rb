bf712eb715163c9e72c264a114255c01c6b70ecc
Michal Cichra <michal@cichra.cz>
2017-09-26 20:29:37 +0200

    raise Model::LockTimeoutError when can't acquire weak lock

--- test/models/model_test.rb
+++ test/models/model_test.rb
@@ -2,7 +2,26 @@
 require 'test_helper'
 
 class ModelTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_weak_lock
+    locking_connection = Model.connection_pool.checkout
+
+    fiber = Model.stub(:connection, locking_connection) do
+      Fiber.new do
+        locking_connection.transaction do
+          Fiber.yield Model.first!.weak_lock
+        end
+      end
+    end
+
+    locked_model = fiber.resume
+    refute_equal Model.connection, locking_connection
+
+    connection = Model.connection_pool.checkout
+
+    Model.stub(:connection, connection) do
+      assert_raises Model::LockTimeoutError do
+        Model.find(locked_model.id).weak_lock
+      end
+    end
+  end
 end

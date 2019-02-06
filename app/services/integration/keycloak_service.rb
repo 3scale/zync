8fa61afd683dce0df8d2156c2c5d0d862e176e27
Marta Noya <mnoyabon@redhat.com>
2019-02-06 18:28:08 +0100

    To be able to remove applications from Keycloak

--- app/services/integration/keycloak_service.rb
+++ app/services/integration/keycloak_service.rb
@@ -38,7 +38,7 @@ class Integration::KeycloakService
     attr_reader :tenant, :client_id, :scope
 
     def initialize(entry)
-      @client_id = entry.data&.dig('client_id')
+      @client_id = entry.data&.dig('client_id') || (Entry.where(model_id: entry.model_id).where.not(data: nil).last&.data || {}).dig('client_id')
       @tenant = entry.tenant
       @scope = Client.for_service(entry.record.service)
     end

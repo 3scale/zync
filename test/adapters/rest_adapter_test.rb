b8f8e6effa220c98a1f7d66e9a4956ba9918be0b
Michal Cichra <michal@cichra.cz>
2019-06-17 10:44:25 +0200

    [rest-api] allow basic auth

--- test/adapters/rest_adapter_test.rb
+++ test/adapters/rest_adapter_test.rb
@@ -10,4 +10,38 @@ class RESTAdapterTest < ActiveSupport::TestCase
 
     assert_nil subject.new('https://example.com').authentication
   end
+
+  test 'create client without auth' do
+    client = RESTAdapter::Client.new(id: 'foo')
+
+    stub_request(:get, "https://example.com/.well-known/openid-configuration").
+      to_return(status: 404, body: '', headers: {})
+
+    stub_request(:put, "https://example.com/clients/foo").
+      with(
+        body: client.to_json,
+        headers: { 'Content-Type'=>'application/json' }).
+      to_return(status: 200, body: { status: 'ok' }.to_json, headers: { 'Content-Type' => 'application/json' })
+
+    assert subject.new('https://example.com').create_client(client)
+  end
+
+  test 'create client with basic auth' do
+    client = RESTAdapter::Client.new(id: 'foo')
+    adapter = subject.new('https://user:pass@example.com')
+    # WebMock does not support request retries on 401 status
+    adapter.send(:http_client).force_basic_auth = true
+
+    stub_request(:get, "https://example.com/.well-known/openid-configuration").
+      to_return(status: 404, body: '', headers: {})
+
+    stub_request(:put, 'https://example.com/clients/foo').
+      with(
+        basic_auth: %w[user pass],
+        body: client.to_json,
+        headers: { 'Content-Type'=>'application/json' }).
+      to_return(status: 200, body: { status: 'ok' }.to_json, headers: { 'Content-Type' => 'application/json' })
+
+    assert adapter.create_client(client)
+  end
 end

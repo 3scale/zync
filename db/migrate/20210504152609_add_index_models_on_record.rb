a6b4d5ee7665e6d544b96b68499079356d544b94
hemahg <hhg@redhat.com>
2021-05-05 20:09:51 +0530

    Incorporating review comment
    Signed-off-by: hemahg <hhg@redhat.com>

--- db/migrate/20210504152609_add_index_models_on_record.rb
+++ db/migrate/20210504152609_add_index_models_on_record.rb
@@ -1,5 +1,6 @@
 class AddIndexModelsOnRecord < ActiveRecord::Migration[6.1]
   def change
-    add_index :models, [:record_id, :record_type], name: "index_models_on_record", if_not_exists: true
+    remove_index :models, name: "index_models_on_record_type_and_record_id" , if_exists: true
+    add_index :models, [:record_id, :record_type], name: "index_models_on_record", if_not_exists: true, unique: true
   end
 end

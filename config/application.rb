780071aee99c7ce5e0f788737231ea822a6577af
Daria Mayorova <mayorova@users.noreply.github.com>
2025-11-18 17:20:05 +0100

    Run rails app:update

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative "boot"
 
 require "rails"
@@ -22,9 +20,13 @@ Bundler.require(*Rails.groups)
 
 module Zync
   class Application < Rails::Application
-
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
 
     # Que needs :sql because of advanced PostgreSQL features
     config.active_record.schema_format = :sql

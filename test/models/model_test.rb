5749073c75fabdd28726033fbaea3ebaf0b18c82
Joan Lledó <jlledo@redhat.com>
2026-01-22 09:41:35 +0100

    Fix tests

--- test/models/model_test.rb
+++ test/models/model_test.rb
@@ -2,26 +2,40 @@
 require 'test_helper'
 
 class ModelTest < ActiveSupport::TestCase
+  self.use_transactional_tests = false
+
   def test_weak_lock
-    locking_connection = Model.connection_pool.checkout
+    model = Model.first!
 
-    fiber = Model.stub(:connection, locking_connection) do
-      Fiber.new do
-        locking_connection.transaction do
-          Fiber.yield Model.first!.weak_lock
-        end
+
+    # The events in the two threads must happen in a particular order:
+    # 1. The new thread must lock the record and wait. This way the lock
+    #    and the transaction are kept open.
+    # 2. The main thread must wait until the lock is taken, only then it can
+    #    try to take the lock
+    # We use queues to signal threads
+    locked_signal = Queue.new
+    release_signal = Queue.new
+
+    # Take the lock and keep it taken
+    thread = Thread.new do
+      Model.transaction do
+        model.weak_lock
+        locked_signal.push(true)
+        release_signal.pop
       end
     end
 
-    locked_model = fiber.resume
-    refute_equal Model.connection, locking_connection
+    # Wait till the lock is taken
+    locked_signal.pop
 
-    connection = Model.connection_pool.checkout
-
-    Model.stub(:connection, connection) do
-      assert_raises Model::LockTimeoutError do
-        Model.find(locked_model.id).weak_lock
-      end
+    # Try to take the lock, we expect it to fail
+    assert_raises Model::LockTimeoutError do
+      model.weak_lock
     end
+
+    # Release the thread
+    release_signal.push(true)
+    thread.join
   end
 end
